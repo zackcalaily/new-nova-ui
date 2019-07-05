061b8293ea493d9d1ab03b468dfa32da4c2fdb1a
Zack Calaily <Zack.Calaily@rci.rogers.ca>
2019-07-05 15:31:36 -0400

    Updates

---
+++ precache-manifest.a14e17dbab1563aa564203ff60eee06c.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231b8b50380e52c939836e3278c2b948",
+    "url": "/new-nova-ui/index.html"
+  },
+  {
+    "revision": "a3824fdf0d35da7dee30",
+    "url": "/new-nova-ui/static/css/main.664e3d0d.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/new-nova-ui/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a3824fdf0d35da7dee30",
+    "url": "/new-nova-ui/static/js/main.d69dccad.chunk.js"
+  },
+  {
+    "revision": "f5374a6254cf3a35e0db",
+    "url": "/new-nova-ui/static/js/runtime~main.617e3a46.js"
+  },
+  {
+    "revision": "099ae3f20a17e18ba3b140a7c119b4b5",
+    "url": "/new-nova-ui/static/media/TedNext-Bold.099ae3f2.woff"
+  },
+  {
+    "revision": "899f4c163f2368ad9d100c61ead4fbd8",
+    "url": "/new-nova-ui/static/media/TedNext-Bold.899f4c16.svg"
+  },
+  {
+    "revision": "d1ee1a8c0010a5e6f558222370d52477",
+    "url": "/new-nova-ui/static/media/TedNext-Bold.d1ee1a8c.woff2"
+  },
+  {
+    "revision": "e8a4a016f51fb5d2c101c2853fd0ec89",
+    "url": "/new-nova-ui/static/media/TedNext-Bold.e8a4a016.eot"
+  },
+  {
+    "revision": "f0d6b4ec294fdf0c05d14c21467dc0e3",
+    "url": "/new-nova-ui/static/media/TedNext-Bold.f0d6b4ec.ttf"
+  },
+  {
+    "revision": "41d7590878a2dcacf5c87df3ecac3a51",
+    "url": "/new-nova-ui/static/media/TedNext-BoldItalic.41d75908.woff"
+  },
+  {
+    "revision": "8b855e6c5dcff6c07537d736e8a4503c",
+    "url": "/new-nova-ui/static/media/TedNext-BoldItalic.8b855e6c.svg"
+  },
+  {
+    "revision": "9192ad6cbfcba373df1545d303c14cc3",
+    "url": "/new-nova-ui/static/media/TedNext-BoldItalic.9192ad6c.eot"
+  },
+  {
+    "revision": "94e52605754e29c1a4a9525164926c9d",
+    "url": "/new-nova-ui/static/media/TedNext-BoldItalic.94e52605.ttf"
+  },
+  {
+    "revision": "e838cf29a5c80a8fc2e5e85b7cf612c6",
+    "url": "/new-nova-ui/static/media/TedNext-BoldItalic.e838cf29.woff2"
+  },
+  {
+    "revision": "25bb3c1e4a697cedf0a37ca8edda80a4",
+    "url": "/new-nova-ui/static/media/TedNext-Italic.25bb3c1e.woff2"
+  },
+  {
+    "revision": "4aaaf425f14fbabcbc7566d8b307b64e",
+    "url": "/new-nova-ui/static/media/TedNext-Italic.4aaaf425.eot"
+  },
+  {
+    "revision": "960416ba6a75dbc77ca761cc6b1e4fe9",
+    "url": "/new-nova-ui/static/media/TedNext-Italic.960416ba.ttf"
+  },
+  {
+    "revision": "b423f118c4487aa48b2665de62e51dc8",
+    "url": "/new-nova-ui/static/media/TedNext-Italic.b423f118.woff"
+  },
+  {
+    "revision": "ee6635caafe9c1beb7a4ac9b88ebddeb",
+    "url": "/new-nova-ui/static/media/TedNext-Italic.ee6635ca.svg"
+  },
+  {
+    "revision": "01fab3c91a697c3e3db5e743fb0c99c0",
+    "url": "/new-nova-ui/static/media/TedNext-Light.01fab3c9.woff2"
+  },
+  {
+    "revision": "0ad2c411cb584e7e34fe6a0d4bfada9b",
+    "url": "/new-nova-ui/static/media/TedNext-Light.0ad2c411.ttf"
+  },
+  {
+    "revision": "80a80f1548f5c9b683530bcde8dc57ff",
+    "url": "/new-nova-ui/static/media/TedNext-Light.80a80f15.svg"
+  },
+  {
+    "revision": "9b55f1870773358916d434e36712cca3",
+    "url": "/new-nova-ui/static/media/TedNext-Light.9b55f187.eot"
+  },
+  {
+    "revision": "fe560099928cc28d2b2c58d9f2fec976",
+    "url": "/new-nova-ui/static/media/TedNext-Light.fe560099.woff"
+  },
+  {
+    "revision": "65ed236067b31328cc145b71caac7d2b",
+    "url": "/new-nova-ui/static/media/TedNext-LightItalic.65ed2360.svg"
+  },
+  {
+    "revision": "c55f1cf6264b4da9d42f9779ea57093e",
+    "url": "/new-nova-ui/static/media/TedNext-LightItalic.c55f1cf6.woff"
+  },
+  {
+    "revision": "c761fe8a5e60867f78ddb1b8da3b91ba",
+    "url": "/new-nova-ui/static/media/TedNext-LightItalic.c761fe8a.eot"
+  },
+  {
+    "revision": "d8800bedcbf9d351e1d95e026d07d130",
+    "url": "/new-nova-ui/static/media/TedNext-LightItalic.d8800bed.ttf"
+  },
+  {
+    "revision": "ea60970fb8db7d7d608ec1b96254ccba",
+    "url": "/new-nova-ui/static/media/TedNext-LightItalic.ea60970f.woff2"
+  },
+  {
+    "revision": "3e933131b46322e75574df2d5267ef7c",
+    "url": "/new-nova-ui/static/media/TedNext-SemiBold.3e933131.eot"
+  },
+  {
+    "revision": "56140bee3582213419f13ef26087bcc1",
+    "url": "/new-nova-ui/static/media/TedNext-SemiBold.56140bee.svg"
+  },
+  {
+    "revision": "75ea8bd8723fdeded3ec1357dbdec6e4",
+    "url": "/new-nova-ui/static/media/TedNext-SemiBold.75ea8bd8.woff"
+  },
+  {
+    "revision": "9fcd046743946dd7fe49373252a15472",
+    "url": "/new-nova-ui/static/media/TedNext-SemiBold.9fcd0467.woff2"
+  },
+  {
+    "revision": "0e22f078fdc69b76ee96a16d9c690ea7",
+    "url": "/new-nova-ui/static/media/TedNext-SemiBoldItalic.0e22f078.woff"
+  },
+  {
+    "revision": "3792eda8282f11ab9b25f345568e4c99",
+    "url": "/new-nova-ui/static/media/TedNext-SemiBoldItalic.3792eda8.eot"
+  },
+  {
+    "revision": "c8fe5b55ac5b1a6aec75baf6c9cc99d9",
+    "url": "/new-nova-ui/static/media/TedNext-SemiBoldItalic.c8fe5b55.woff2"
+  },
+  {
+    "revision": "d3351466ed1fae9e2642e4243b4e5f26",
+    "url": "/new-nova-ui/static/media/TedNext-SemiBoldItalic.d3351466.svg"
+  },
+  {
+    "revision": "eafbf3609b0fdf9b8f624a63e3993142",
+    "url": "/new-nova-ui/static/media/TedNext-SemiBoldItalic.eafbf360.ttf"
+  },
+  {
+    "revision": "15ba45cd9cc8965e1752d9260e3560c8",
+    "url": "/new-nova-ui/static/media/TedNext.15ba45cd.eot"
+  },
+  {
+    "revision": "64781ae0bb83f59891eb70e98ec99266",
+    "url": "/new-nova-ui/static/media/TedNext.64781ae0.svg"
+  },
+  {
+    "revision": "7d480456ceb774b282287f7c4bd106cf",
+    "url": "/new-nova-ui/static/media/TedNext.7d480456.ttf"
+  },
+  {
+    "revision": "83b5527b310e43b571ed876a714667c0",
+    "url": "/new-nova-ui/static/media/TedNext.83b5527b.woff"
+  },
+  {
+    "revision": "b846c33a1486c09bc8f9c7ab69da80b2",
+    "url": "/new-nova-ui/static/media/TedNext.b846c33a.woff2"
+  }
+]);
